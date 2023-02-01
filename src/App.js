4a25160a9f83d79ec327506817a52aaf57b76bb6
James Chen <zhaojun.chen0716@gmail.com>
2023-02-01 13:50:43 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState([]);
+
+  useEffect(() => {
+    const getAPI = async () => {
+      const response = await fetch(
+        `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=sacramento&apikey=${process.env.REACT_APP_TICKETMASTER_KEY}`
+      );
+      const result = await response.json();
+      console.log(result._embedded.events);
+      setTitle(result._embedded.events);
+    };
+
+    getAPI();
+  }, []);
+
+  const render = () => {
+    console.log(title);
+    title.map((event) => {
+      return <h1>{event.name}</h1>;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={render}>run</button>
+      {title.map((event) => {
+        return <h1>{event.name}</h1>;
+      })}
     </div>
   );
 }
